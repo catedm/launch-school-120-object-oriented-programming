2570f872ed74a0eba728de8d9fc1666e8a6ad169
David Cate <david@element-360.com>
2017-02-26 18:47:00 -0500

    book commits

---
+++ oop_book_exercises/classes_objects_2/ex_1.rb
@@ -0,0 +1,51 @@
+class MyCar
+
+  attr_accessor :color
+  attr_reader :year
+
+  def self.gas_milage(gallons, miles)
+    puts "#{miles / gallons} miles per gallon of gas"
+  end
+
+  def initialize(y, c, m)
+    @year = y
+    @color = c
+    @model = m
+    @current_speed = 0
+  end
+
+  def speed_up(mph)
+    @current_speed += mph
+    puts "You push the gas and accelerate #{mph} mph."
+  end
+
+  def current_speed
+    puts "You are currently going #{@current_speed} mph."
+  end
+
+  def brake(mph)
+    @current_speed -= mph
+    puts "You push the brake and decelerate #{mph} mph."
+  end
+
+  def turn_off
+    @current_speed = 0
+    puts "Let's park this bad boy."
+  end
+
+  def spray_paint(color)
+    self.color = color
+    puts "You just spray painted your car #{color}, and it looks awful."
+  end
+end
+
+MyCar.gas_milage(13, 351)
+billy = MyCar.new(2002, "red", "Blazer")
+billy.speed_up(25)
+billy.brake(10)
+billy.current_speed
+billy.color = "Green"
+puts billy.color
+puts billy.year
+billy.spray_paint("Black")
+puts billy.color
