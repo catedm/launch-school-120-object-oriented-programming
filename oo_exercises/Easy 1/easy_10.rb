656c55a4920731ee1e6275aa586680ea4ba6295a
David Cate <david@element-360.com>
2017-03-13 14:48:21 -0400

    oo exercises

---
+++ oo_exercises/Easy 1/easy_10.rb
@@ -0,0 +1,44 @@
+class Vehicle
+  attr_reader :make, :model
+
+  def initialize(make, model)
+    @make = make
+    @model = model
+  end
+
+  def to_s
+    "#{make} #{model}"
+  end
+end
+
+class Car < Vehicle
+  def wheels
+    4
+  end
+end
+
+class Motorcycle < Vehicle
+  def wheels
+    2
+  end
+end
+
+class Truck < Vehicle
+  attr_reader :payload
+
+  def initialize(make, model, payload)
+    super(make, model)
+    @payload = payload
+  end
+
+  def wheels
+    6
+  end
+
+  def to_s
+    "#{make} #{model} #{payload}"
+  end
+end
+
+truck1 = Truck.new("Toyota", "Tacoma", 123)
+puts truck1
